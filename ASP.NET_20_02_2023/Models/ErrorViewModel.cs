e1f11e8bcbc0e3f172020d932ed656d0e1f5dde9
106 <106@106-12>
2023-02-20 10:02:19 +0100

    Dodaj pliki projektów.

---
+++ ASP.NET_20_02_2023/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_20_02_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
