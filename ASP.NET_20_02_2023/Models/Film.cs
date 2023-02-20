60e2856f0540951028079318108c23b93933d33a
Igor Gruszczyński <95656@office365.ukw.edu.pl>
2023-02-20 10:49:57 +0100

    Dodaje klasy Film i Kategoria

---
+++ ASP.NET_20_02_2023/Models/Film.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_20_02_2023.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Podaj tytul")]
+        [StringLength(50)]
+        public string Tytul { get; set; }
+
+        [Required(ErrorMessage = "Podaj rezysera")]
+        [StringLength(50)]
+        public string Rezyser { get; set; }
+
+        [Required(ErrorMessage = "Podaj opis")]
+        [StringLength(200)]
+        public string Opis { get; set; }
+        public decimal? Cena { get; set; }
+        public DateTime DataDodania { get; set; }
+
+        [ForeignKey("Kategoria")]
+        public int KategoriaId { get; set; }
+        public Kategoria Kategoria { get; set; }
+    }
+}
---
+++ ASP.NET_20_02_2023/Models/Kategoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_20_02_2023.Models
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public int Opis { get; set; }
+
+        public ICollection<Film> Filmy { get; set; }
+    }
+}
