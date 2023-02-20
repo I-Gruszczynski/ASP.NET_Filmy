0e9593a6ac5ef8c99c71f11834cc0e4847efb68e
Igor Gruszczyński <95656@office365.ukw.edu.pl>
2023-02-20 10:32:51 +0100

    kom

--- ASP.NET_20_02_2023/Program.cs
+++ ASP.NET_20_02_2023/Program.cs
@@ -14,6 +14,7 @@ namespace ASP.NET_20_02_2023
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //test
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
