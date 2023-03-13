8ba352a632966581c904ad29315fe05322cb39c8
Igor Gruszczyński <95656@office365.ukw.edu.pl>
2023-03-13 11:13:45 +0100

    List and menu

---
+++ ASP.NET_20_02_2023/Controllers/FilmyController.cs
@@ -0,0 +1,30 @@
+﻿using ASP.NET_20_02_2023.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_20_02_2023.Controllers
+{
+    public class FilmyController : Controller
+    {
+        FilmyContext db;
+        public FilmyController(FilmyContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Lista(string nazwaKategorii)
+        {
+            var kategoria = db.Kategorie.Include("Filmy").Where(k => k.Nazwa.ToUpper() == nazwaKategorii).Single();
+
+            var filmy = kategoria.Filmy.ToList();
+
+
+            //ViewBag.nazwa;
+            return View(filmy);
+        }
+    }
+}
