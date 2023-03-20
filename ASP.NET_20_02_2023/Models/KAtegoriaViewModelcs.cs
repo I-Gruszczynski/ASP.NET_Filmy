3422b89fd4206ad84ebbc206e467e68a0c6b2ad4
Igor Gruszczyński <95656@office365.ukw.edu.pl>
2023-03-20 09:55:46 +0100

    ff

---
+++ ASP.NET_20_02_2023/Models/KAtegoriaViewModelcs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_20_02_2023.Models
+{
+    public class KAtegoriaViewModelcs
+    {
+    }
+}
