2cf3bc20c7222d17fd9d0c3e2c03c5e0b73ca76b
Igor Gruszczyński <95656@office365.ukw.edu.pl>
2023-05-08 10:22:41 +0200

    merge z main

---
+++ ASP.NET_20_02_2023/Infrastructure/MenadzerKoszyka.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_20_02_2023.Infrastructure
+{
+    public class MenadzerKoszyka
+    {
+        public static int UsunZKoszyka(ISession session, int id)
+        {
+            var koszyk
+        }
+    }
+}
